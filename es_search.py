36623fa4376bbc3d8e3b9335c56337326c193daf
mgoddard <mgoddard@cockroachlabs.com>
2022-07-07 09:00:08 -0400

    Elasticsearch search script in Python

---
+++ es_search.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys, os
+import requests
+import urllib3
+import json
+
+# Suppress warnings when using requests with "verify=False" (e.g. no SSL validation)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+es_user = os.getenv("ES_USER")
+es_passwd = os.getenv("ES_PASSWD")
+es_url = os.getenv("ES_URL")
+es_index = os.getenv("ES_INDEX", "defaultdb") # CockroachDB database maps to Elastic index
+
+if len(sys.argv) < 2:
+  print("Usage: {} your query string ...".format(sys.argv[0]))
+  sys.exit(1)
+
+q = json.loads("""{ 
+  "_source": false,
+  "query": {
+    "match_phrase": {
+      "content": ""
+    }
+  },
+  "highlight": {
+    "fields": {
+      "content" : { "fragment_size" : 80, "number_of_fragments" : 4 }
+    }
+  }
+}""")
+q["query"]["match_phrase"]["content"] = ' '.join(sys.argv[1:])
+
+headers = {"Content-Type": "application/json"}
+r = requests.post('/'.join([es_url, es_index, "_search"]), auth=(es_user, es_passwd), verify=False, json=q, headers=headers)
+print(json.dumps(r.json(), sort_keys=True, indent=2))
+
